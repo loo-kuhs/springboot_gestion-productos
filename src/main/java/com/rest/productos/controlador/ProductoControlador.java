a2282f1f8ac99b91ffe18a2db33e16d1333c6f08
lucasbear <rukazu89@gmail.com>
2023-01-23 00:07:20 -0600

    Added edit and delete item functionality

--- src/main/java/com/rest/productos/controlador/ProductoControlador.java
+++ src/main/java/com/rest/productos/controlador/ProductoControlador.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
 import com.rest.productos.entidades.Producto;
 import com.rest.productos.servicio.ProductoServicio;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -37,5 +39,13 @@ public class ProductoControlador {
 	public String guardarProducto(@ModelAttribute("producto") Producto producto) {
 		productoServicio.save(producto);
 		return "redirect:/";
+	}
+	
+	@RequestMapping("/editar/{id}")
+	public ModelAndView mostrarFormularioDeEditarProducto(@PathVariable(name = "id") Long id) {
+		ModelAndView modelo = new ModelAndView("editar_producto");
+		Producto producto = productoServicio.get(id);
+		modelo.addObject("producto", producto);
+		return modelo;
 	}
 }
